44e82040824ff2885b23fb9c4dc08efb1a2a07df
Yash Sharma <104293480+yash-sharma-007@users.noreply.github.com>
2023-12-21 22:49:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
